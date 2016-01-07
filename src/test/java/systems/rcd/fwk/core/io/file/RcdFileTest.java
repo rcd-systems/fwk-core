bbacc197d4c48d9436a91ab4814b50b54bd44d14
RcdSystems <git.rcd.systems@gmail.com>
2016-01-08 00:58:24 +0100

    File service test

--- src/test/java/systems/rcd/fwk/core/io/file/RcdFileTest.java
+++ src/test/java/systems/rcd/fwk/core/io/file/RcdFileTest.java
@@ -13,24 +13,15 @@ import org.junit.Test;
 public class RcdFileTest {
 
     private static final String FILE_DIRECTORY = "src/test/resources/systems/rcd/fwk/core/io/file";
-    private static final Path UNIX_PATH = Paths.get(FILE_DIRECTORY, "input-unix.txt");
-    private static final Path WIN_PATH = Paths.get(FILE_DIRECTORY, "input-win.txt");
+    private static final Path INPUT_PATH = Paths.get(FILE_DIRECTORY, "input.txt");
 
     @Test
     public void testRcdNioFileService() throws Exception {
-        final String unixContentAsString = RcdFileService.readAsString(UNIX_PATH);
+        final String unixContentAsString = RcdFileService.readAsString(INPUT_PATH);
         Assert.assertEquals("Some content\nこんにちわ\næøå", unixContentAsString);
-        final String winContentAsString = RcdFileService.readAsString(WIN_PATH);
-        Assert.assertEquals("Some content\r\nこんにちわ\r\næøå", winContentAsString);
 
         final List<String> expectedList = Arrays.asList(new String[] { "Some content", "こんにちわ", "æøå" });
-
-        RcdFileService.readAsStream(UNIX_PATH, lines -> {
-            final List<String> actualList = lines.collect(Collectors.toList());
-            Assert.assertEquals(expectedList, actualList);
-        });
-
-        RcdFileService.readAsStream(WIN_PATH, lines -> {
+        RcdFileService.readAsStream(INPUT_PATH, lines -> {
             final List<String> actualList = lines.collect(Collectors.toList());
             Assert.assertEquals(expectedList, actualList);
         });
