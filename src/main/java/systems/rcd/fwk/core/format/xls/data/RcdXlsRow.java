dde60a2db2fc670fab48e58ca15f7b0a2ef4ba1b
RcdSystems <git.rcd.systems@gmail.com>
2016-01-15 19:01:36 +0100

    Get Value from XlsRow

--- src/main/java/systems/rcd/fwk/core/format/xls/data/RcdXlsRow.java
+++ src/main/java/systems/rcd/fwk/core/format/xls/data/RcdXlsRow.java
@@ -1,7 +1,15 @@
 package systems.rcd.fwk.core.format.xls.data;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface RcdXlsRow extends List<RcdXlsCell> {
+    String getString(int index);
+
+    Instant getInstant(int index);
+
+    Double getNumber(int index);
+
+    Boolean getBoolean(int index);
 
 }
