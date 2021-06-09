7adc7134f52fdcae1fbf2273d9a6e67f99d4bf94
rcd-systems <comptepoubelle8@gmail.com>
2021-06-09 18:54:04 +0200

    NashornScriptEngineFactory

--- src/main/java/systems/rcd/fwk/core/script/js/impl/RcdSimpleJavascriptService.java
+++ src/main/java/systems/rcd/fwk/core/script/js/impl/RcdSimpleJavascriptService.java
@@ -1,22 +1,28 @@
 package systems.rcd.fwk.core.script.js.impl;
 
-import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import systems.rcd.fwk.core.exc.RcdException;
 import systems.rcd.fwk.core.script.js.RcdJavascriptService;
 
 public class RcdSimpleJavascriptService
     implements RcdJavascriptService
 {
+    private final ScriptEngine scriptEngine;
+
+    public RcdSimpleJavascriptService()
+    {
+        this.scriptEngine = new NashornScriptEngineFactory().getScriptEngine();
+    }
+
     @Override
     public Object instEval( final String script )
     {
         try
         {
-            return new ScriptEngineManager().
-                getEngineByName( "nashorn" ).
-                eval( script );
+            return scriptEngine.eval( script );
         }
         catch ( ScriptException e )
         {
